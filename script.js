36546a779952fb99228bce1fb0871226fd22b45f
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2025-04-22 21:19:42 +0800

    ✍

--- script.js
+++ script.js
@@ -9,4 +9,26 @@ document.addEventListener('DOMContentLoaded', () => {
     cells.forEach(cell => {
         new Eye(cell); // Assuming Eye class handles the eye creation
     });
+
+    // Torch  
+    const torch = document.getElementById('torch-overlay');
+
+    document.addEventListener('mousemove', e => {
+    const x = `${e.clientX}px`;
+    const y = `${e.clientY}px`;
+    torch.style.setProperty('--x', x);
+    torch.style.setProperty('--y', y);
+    });
+
+    document.body.addEventListener('mouseenter', () => {
+        torch.classList.remove('mouse-leave'); 
+    });
+
+    document.body.addEventListener('mouseleave', () => {
+        torch.classList.add('mouse-leave');
+    });
+
+    document.addEventListener('mouseenter', () => {
+        torch.classList.remove('mouse-leave');
+    });
 });
